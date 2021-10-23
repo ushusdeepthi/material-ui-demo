955affce01c8e1b4b05b46aea192b6fdb675ebe1
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-23 19:03:45 +0200

    :truck: Move styling to a separate file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import {Button,Typography,AppBar,Card, CardActions,CardMedia, CssBaseline,Grid,Toolbar,Container} from '@material-ui/core'
 import { PhotoCamera } from '@material-ui/icons'
-import {makeStyles} from '@material-ui/core/styles'
-
-const useStyles = makeStyles((theme)=>({
-  container:{
-    backgroundColor:theme.palette.background.paper,
-    padding:theme.spacing(8,0,6)
-  }
-})) 
-
+import useStyles from './styles'
 
 export default function App() {
   const classes = useStyles()
